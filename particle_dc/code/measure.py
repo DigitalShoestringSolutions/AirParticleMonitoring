f30e566cd21d293e19b2b4e09ae4a29fdc758be7
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-09-12 10:57:47 +0100

    Merged calculate.py into measure.py

--- particle_dc/code/measure.py
+++ particle_dc/code/measure.py
@@ -118,10 +118,18 @@ class AirParticleMeasureBuildingBlock(multiprocessing.Process):
             timestamp = datetime.datetime.now(tz=tz).isoformat()
 
             # convert
-            results = calculation.calculate(sample)
-            payload = {**results, **self.constants, "timestamp": timestamp}
+            results =  {"mc_1p0": sample.mass_concentration_1p0.physical,
+                        "mc_2p5": sample.mass_concentration_2p5.physical,
+                        "mc_4p0": sample.mass_concentration_4p0.physical,
+                        "mc_10p0": sample.mass_concentration_10p0.physical,
+                        "ambient_rh": sample.ambient_humidity.percent_rh,
+                        "ambient_t": sample.ambient_temperature.degrees_celsius,
+                        "voc_index": sample.voc_index.scaled,
+                        "nox_index": sample.nox_index.scaled,
+                       }
 
             # send
+            payload = {**results, **self.constants, "timestamp": timestamp}
             output = {"path": "", "payload": payload}
             self.dispatch(output)
 
